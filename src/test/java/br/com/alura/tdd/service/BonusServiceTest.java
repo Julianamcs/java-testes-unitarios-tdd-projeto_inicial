f4eb587f387ea3212c9c496273d155de1da5ddc4
Juliana Maciel <julyanamcs@gmail.com>
2023-07-20 05:36:00 -0300

    Teste Unidade

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.alura.tdd.service;
 
+import static java.math.BigDecimal.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import br.com.alura.tdd.modelo.Funcionario;
@@ -12,11 +13,26 @@ class BonusServiceTest {
 
 	@Test
 	void bonusDeveriaSerZeroparaFuncionarioComSalarioMuitoAlto() {
-
 		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("Ana da Silva", LocalDate.now(), new BigDecimal("25000")));
+		assertEquals(new BigDecimal("0.00"), bonus);
+		System.out.println(bonus);
+	}
 
-		assertEquals(BigDecimal.ZERO, bonus);
+	@Test
+	void bonusDeveriaSer10PorcentoDoSalario(){
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Ana da Silva", LocalDate.now(), new BigDecimal("2500")));
+		assertEquals(new BigDecimal("250.00"), bonus);
+		System.out.println(bonus);
+	}
+
+	@Test
+	void bonusDeveriaSerDezPorcentoParaSalarioDeExatamente10000(){
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Ana da Silva", LocalDate.now(), new BigDecimal("10000")));
+		assertEquals(new BigDecimal("1000.00"), bonus);
+		System.out.println(bonus);
 	}
 
 }
